9410a97529595aae389535dee4a269b8dbba76d6
coreindustries <cszopinski@cpbgroup.com>
2013-04-04 22:09:55 -0600

    simple working socket.io clock

--- www/server.js
+++ www/server.js
@@ -1,11 +1,9 @@
-var io = require('socket.io').listen(8001);
+var io = require('socket.io').listen(80);
 
 io.sockets.on('connection', function (socket) {
   socket.emit('hello', { hello: 'world' });
-  
   var t = setInterval(function(){
-  	var d = new Date();
-  	socket.emit('time', { time: d });
+  	socket.emit('time', { time: 'time' });
   }, 1000);
   socket.on('my other event', function (data) {
     console.log(data);
