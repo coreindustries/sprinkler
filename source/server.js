790fc64ffa17c276f835be824c710bea6edd4339
coreindustries <cszopinski@cpbgroup.com>
2013-04-04 23:10:02 -0600

    button toggles

--- source/server.js
+++ source/server.js
@@ -1,14 +1,16 @@
 var io = require('socket.io').listen(8001);
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('hello', { hello: 'world' });
-  
-  var t = setInterval(function(){
-  	var d = new Date();
-  	socket.emit('time', { time: d });
-  }, 1000);
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+	// socket.emit('hello', { hello: 'world' });
+	// var d = new Date();
+	// socket.emit('time', { time: d });
+
+	var t = setInterval(function(){
+		var d = new Date();
+		socket.emit('time', { time: d });
+	}, 500);
+	socket.on('my other event', function (data) {
+	console.log(data);
+	});
 });
 
